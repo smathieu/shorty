37524b9ef0663ce57e4bb15091c81584c4ce69ce
Simon Mathieu <simon.mathieu@lookout.com>
2016-04-05 22:43:20 -0700

    Heroku Prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :shorty, Shorty.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: System.get_env("HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :shorty, Shorty.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # ## SSL Support
 #
